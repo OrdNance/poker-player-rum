1c6f03a813a930797d5cc4b386dbd790cb5e9356
DBiedermann <DBiedermann@ADC-NB10650.intern.cube.ch>
2017-06-29 11:19:55 +0200

    fdfdsa

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -9,7 +9,7 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-        return 10000;
+        return 100000;
     }
 
     public static void showdown(JsonElement game) {
